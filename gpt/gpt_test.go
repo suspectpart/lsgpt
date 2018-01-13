323495b4fbec9c0dd71da8c3f7a5cab8043d609a
Lars Kumbier <lars@kumbier.it>
2018-01-13 12:01:17 +0100

    Adds test for a broken EFI signature

--- gpt/gpt_test.go
+++ gpt/gpt_test.go
@@ -7,8 +7,8 @@ import (
 	"github.com/suspectpart/lsgpt/gpt"
 )
 
-func TestReadHeader(t *testing.T) {
-	testfileOk := "gpt_test_hdok"
+func Test_ShouldReadHeader(t *testing.T) {
+	testfileOk := "gpt_test_hd"
 	expectedUUID := "17b49718-e953-4465-8eba-0b6ca15d0ebd"
 
 	header, err := gpt.ReadHeader(testfileOk)
@@ -20,5 +20,14 @@ func TestReadHeader(t *testing.T) {
 	actualUUID := fmt.Sprintf("%s", header.DiskGUID.AsUUID())
 	if actualUUID != expectedUUID {
 		t.Errorf("gpt.ReadHeader(%q): Actual %s != expected %s", testfileOk, actualUUID, expectedUUID)
+	}
+}
+
+func Test_ShouldBreakOnBrokenEFISignature(t *testing.T) {
+	testfile := "gpt_test_hd_brokenEfiSignature"
+	_, err := gpt.ReadHeader(testfile)
+
+	if err == nil {
+		t.Errorf("gpt.ReadHeader(%q) should throw an error", testfile)
 	}
 }
